4fed13baf28fc20e9831d8aa9c63c93dfe01b01f
Juan Esteban Berrío <juanestebanberrio@gmail.com>
2022-07-12 22:49:24 -0500

    adminLTE commit

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware(['auth:sanctum',config('jetstream.auth_session'),
 // 'verified' ])->group(function () {Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');});
 
-Route::get('/',[HomeController::class,'index'])->name('bane');
+Route::middleware(['auth:sanctum','verified'])->get('/',[HomeController::class,'index'])->name('name');
 
